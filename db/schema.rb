1befdcc2883bfb88748a284b020120ed4d3fda0a
Conner Simmering <connersimmering@gmail.com>
2016-06-15 16:37:05 -0600

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20160615221040) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
